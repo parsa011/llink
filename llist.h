ba178aef7fc44797aa3d709aad9c03bf5b5cd894
parsa011 <parsa32569@gmail.com>
2022-01-14 19:08:30 +0330

    set prev and next done , and write some test

--- llist.h
+++ llist.h
@@ -6,22 +6,21 @@
  * 	parsa mahmoudy
  */
 
-#define L_LINK(type) struct { type *next; type *prev }
+#define L_LINK(type) struct { type *next; type *prev; }
 
-#define L_LINK_NEXT(o) (o->link.next)
-#define L_LINK_PREV(o) (o->link.prev)
-#define L_LINK_SNEXT(s,d) (s->link.next = d)
-#define L_LINK_SPREV(s,d) (s->link.prev = d)
+#define L_LINK_NEXT(o) ((o)->link.next)
+#define L_LINK_PREV(o) ((o)->link.prev)
+#define L_LINK_SNEXT(s,d) ((s)->link.next = d)
+#define L_LINK_SPREV(s,d) ((s)->link.prev = d)
 
-#define L_LINK_APPEND(l,n) 
-	{ \ 
-		void *next = L_LINK_NEXT(l); \ 
-		L_LINK_SNEXT(l,n); \ 
-		L_LINK_SPREV(n,l) \
-		if (next == NULL) { \
-			L_LINK_SPREV(next,n); \
-		    L_LINK_SNEXT(n,next);
-		} \
+#define L_LINK_APPEND(l,n) {\
+		void *next = L_LINK_NEXT(l);\
+		L_LINK_SNEXT(l,n);\
+		L_LINK_SPREV(n,l);\
+		if (next == NULL) {\
+			L_LINK_SPREV(next,n);\
+		    L_LINK_SNEXT(n,next);\
+		}\
 	}
 
 #endif
--- test/test.c
+++ test/test.c
@@ -1,9 +1,76 @@
 #include "../llist.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <string.h>
+
+#define DEFAULT "\033[0m"
+#define FAILED "\033[0;31m"
+#define SUCCESS "\033[0;32m"
+
+typedef struct person_t person;
+
+typedef struct person_t {
+	L_LINK(person) link;
+	char name[64];
+} person;
+
+bool test_set_next()
+{
+	person a;
+	strcpy(a.name,"1"); 
+
+	person b;
+	strcpy(b.name,"2"); 
+	L_LINK_SNEXT(&a,&b);
+
+	return strcmp(L_LINK_NEXT(&a)->name,"2") == 0;
+}
+
+bool test_set_prev()
+{
+	person a;
+	strcpy(a.name,"1"); 
+
+	person b;
+	strcpy(b.name,"2"); 
+	L_LINK_SPREV(&b,&a);
+
+	return strcmp(L_LINK_PREV(&b)->name,"1") == 0;
+}
+
+const char *test_functions_names[] = {
+	"test set next",
+	"test set prev"
+};
+
+bool (*test_functions[])() = {
+	test_set_next,
+	test_set_prev
+};
+
+void log_res(char *state,const char *fmt,...)
+{
+	printf("%s",state);
+	char msg[256];
+	va_list ap;
+	va_start(ap,fmt);
+	vsnprintf(msg,256,fmt,ap);
+	va_end(ap);
+	printf("result : %s%s",DEFAULT,msg);
+	putchar('\n');
+}
 
 int main()
 {
-	printf("working");
+	int test_func_count = sizeof(test_functions) / sizeof(test_functions[0]);
+	for (int i = 0;i < test_func_count;i++) {
+		if ((*test_functions[i])() == true) {
+			log_res(SUCCESS,"'%s' passed",test_functions_names[i]);
+		} else {
+			log_res(FAILED,"'%s' failed",test_functions_names[i]);
+		}
+	}
 	return 0;
 }
